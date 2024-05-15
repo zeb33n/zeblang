b90da7e60170ed4f2ac64f8bc07a6ebe0ba92caa
zeb33n <zeb.bond@33n.co.uk>
2024-05-15 14:26:24 +0100

    added new test

--- src/tests/integration_tests.rs
+++ src/tests/integration_tests.rs
@@ -1,3 +1,4 @@
+use std::fs::read_to_string;
 use std::process::Command;
 
 fn extract_value_from_stdout(stdout: &Vec<u8>) -> u8 {
@@ -12,11 +13,23 @@ fn extract_value_from_stdout(stdout: &Vec<u8>) -> u8 {
 
 fn run_zeblang_file(addr: &str) -> u8 {
     let output = Command::new("bash")
-        .arg("test_compile.sh")
+        .arg("test_scripts/test_compile.sh")
         .arg(addr)
         .output()
         .expect("failed");
     extract_value_from_stdout(&output.stdout)
+}
+
+fn run_zeblang_file_json(addr: &str) -> String {
+    let _ = Command::new("bash")
+        .arg("test_scripts/test_json.sh")
+        .arg(addr)
+        .output();
+    let out = read_to_string(addr.replace(".zb", ".json")).expect("failed");
+    let _ = Command::new("rm")
+        .arg(addr.replace(".zb", ".json"))
+        .output();
+    out
 }
 
 #[test]
@@ -53,4 +66,37 @@ fn test_precedance() {
 fn test_variables() {
     let out = run_zeblang_file("test_scripts/variables.zb");
     assert_eq!(3, out);
+}
+
+#[test]
+fn test_json_syntax_error() {
+    let out = run_zeblang_file_json("test_scripts/syntax_error.zb");
+    assert_eq!("\"syntax error: expected operator\"".to_string(), out);
+}
+
+#[test]
+fn test_json() {
+    let out = run_zeblang_file_json("test_scripts/json.zb");
+    assert_eq!(
+        "[\n  {\n    \"Assign\": [\n      \"var\",\n      {\n        \"Array\": [\n          {\n            \"Value\": \"1\"\n          },\n          {\n            \"Value\": \"2\"\n          },\n          {\n            \"Value\": \"3\"\n          }\n        ]\n      }\n    ]\n  },\n  {\n    \"Exit\": {\n      \"Var\": \"var\"\n    }\n  }\n]", 
+        out,
+    );
+}
+
+#[test]
+fn test_print() {
+    let output = Command::new("bash")
+        .arg("test_scripts/test_compile.sh")
+        .arg("test_scripts/print.zb")
+        .output()
+        .expect("failed");
+    let out_slice = &String::from_utf8_lossy(&output.stdout)
+        .replace("\n", "")
+        .rsplit_once("...")
+        .unwrap()
+        .1
+        .rsplit("\0")
+        .map(|s| s.to_string())
+        .collect::<Vec<_>>()[1..6];
+    assert_eq!(out_slice, ["011", "010", "321", "201", "001"])
 }
