1be4523c50dc23c9a8af79be155e5c389fb87509
zeb33n <zeb.bond@33n.co.uk>
2025-01-20 18:38:20 +0000

    this is the way

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,9 @@ use tokenizer::Lexer;
 
 mod error;
 
-pub mod parser;
-pub use self::parser::ExpressionNode;
-pub use self::parser::StatementNode;
+mod parser;
 use parser::parse;
+pub use parser::{ExpressionNode, StatementNode};
 
 pub fn make_parsetree(src: String) -> Result<Vec<StatementNode>> {
     let lines: Vec<String> = src.lines().into_iter().map(|s| s.to_string()).collect();
