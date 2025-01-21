b064a285a726ecc2942eb39103da28bed7fd65ce
zeb33n <zeb.bond@33n.co.uk>
2025-01-21 20:45:35 +0000

    clone

--- src/parser.rs
+++ src/parser.rs
@@ -8,7 +8,7 @@ use std::vec::IntoIter;
 
 use serde::Serialize;
 
-#[derive(Debug, Serialize, PartialEq)]
+#[derive(Debug, Serialize, PartialEq, Clone)]
 pub enum StatementNode {
     Return(ExpressionNode),
     Exit(ExpressionNode),
@@ -24,7 +24,7 @@ pub enum StatementNode {
     EndFunc,
 }
 
-#[derive(Debug, Serialize, PartialEq)]
+#[derive(Debug, Serialize, PartialEq, Clone)]
 pub enum ExpressionNode {
     Value(String),
     Var(String),
